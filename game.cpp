1e92632dd75111ccd462464e9de398ba7d5eecf4
isaacwu6a25 <isaacwu6a25@gmail.com>
2021-12-03 19:19:37 +0800

    some progress

--- game.cpp
+++ game.cpp
@@ -1,13 +1,11 @@
 // board.cpp
 #include "game.h" // header in local directory
 #include <iostream> // header in standard library
-#include <array>
-#include <vector>
 
 using namespace std;
 
 Game::Game(void) {
-    struct GameState init_state;
+    GameState init_state;
 
     for (int i=0; i<init_state.board.size(); i++) {
         init_state.board[i] = EMPTY;
@@ -17,12 +15,20 @@ Game::Game(void) {
     init_state.player = WHITE;
 
     game_hist.push_back(init_state);
+
+    fill_chi_search();
 }
 
 bool Game::make_move(move_t move) {
+    GameState new_state;
 
+    if (!valid_without_rules(move)) {
+        return false;
+    }
+    new_state.copy_state(&game_hist.back());
 }
 
+// test whether move is on board and not overlapping stones
 bool Game::valid_without_rules(move_t move) {
     if (move < RESIGN || move >= NUM_INTERSECTIONS) {
         return false;
@@ -33,17 +39,39 @@ bool Game::valid_without_rules(move_t move) {
     return true;
 }
 
-bool Game::has_air(move_t move) {
-    int xpos, ypos;
-    xpos = n2posx(move); ypos = n2poxy(move);
+// generates a map of liberties
+void Game::gen_chi_map(move_t move) {
+    
 
-    if (xpos > 1 && xpos < SIZE && ypos > 1 && ypos < SIZE) {
-        if (game_hist.back().board[move-1] != EMPTY ||
-            game_hist.back().board[move+1] != EMPTY) {
-        }
-    }
-
-    return false;
+    return;
 }
 
+void Game::fill_chi_search(void) {
+    int x, y, std_chi[4] = {1, 19, -1, -19};
 
+    for (int n=0; n<NUM_INTERSECTIONS; n++) {
+        for (int m=0; m<4; m++) {
+            chi_search[n][m] = std_chi[m];
+        }
+        x = n%SIZE; y = n/SIZE;
+        if (x==SIZE-1) {
+            chi_search[n][0] = 0;
+        } else if (x==0) {
+            chi_search[n][2] = 0;
+        }
+        if (y==SIZE-1) {
+            chi_search[n][1] = 0;
+        } else if (y==0) {
+            chi_search[n][3] = 0;
+        }
+    }
+}
+
+void GameState::copy_state(GameState* game_state) {
+    player = game_state->player;
+    move_num = game_state->move_num;
+    move = game_state->move;
+    for (int n=0; n<NUM_INTERSECTIONS; n++) {
+        board[n] = game_state->board[n];
+    }
+}
--- game.h
+++ game.h
@@ -2,14 +2,17 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
+#include <array>
+#include <vector>
+
 using namespace std;
 
 // board #defines
 #define SIZE                19
 #define NUM_INTERSECTIONS   SIZE * SIZE
-#define BLACK               0
-#define WHITE               1
-#define EMPTY               -1
+#define BLACK               1
+#define WHITE               -1
+#define EMPTY               0
 
 // game #defines
 #define KOMI                7.5
@@ -22,11 +25,15 @@ using namespace std;
 typedef int move_t;
 typedef std::array<int, NUM_INTERSECTIONS> board_t;
 
-struct GameState{
+class GameState{
+
+    public:
     int player;
     int move_num;
     int move;
     board_t board;
+
+    void copy_state(GameState* game_state);
 };
 
 class Game {
@@ -36,14 +43,27 @@ class Game {
 
     // initiate game with empty board
     Game(void);
+
     // to make a move
     bool make_move(move_t move);
 
     private:
+    // make sure move is placeable
     bool valid_without_rules(move_t move);
 
-    bool liberty_rules(move_t move);
-    bool has_air(move_t move);
+    // to search for liberties
+    int chi_search[NUM_INTERSECTIONS][4];
+    void fill_chi_search(void);
+
+    // generate a map of liberties
+    int chi_map[NUM_INTERSECTIONS];
+    void gen_chi_map(move_t move);
+
+    // generate a map of groups
+    int group_map[NUM_INTERSECTIONS];
+    void gen_group_map();
+    void find_same_group();
+
     bool is_suicide(move_t move);
 
     bool simple_ko(move_t move);
